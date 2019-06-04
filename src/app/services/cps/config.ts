1a31122b0b44f47c85df4f8c5bf03a31c8d76e9f
zengqiang <769729647@qq.com>
2019-06-04 09:39:03 +0800

    配置文件中的url新增type

--- src/app/services/cps/config.ts
+++ src/app/services/cps/config.ts
@@ -1,10 +1,5 @@
-import {environment} from '../../../environments/environment';
 
 const domain = 'http://bms.21thedu.com/index.php';
-
-/*if (environment.production) {
-  domain = 'http://bms.xhz.com/index.php';
-}*/
 
 export const Config = {
   getOrgUrl: domain + '?m=admin&c=weixin&a=get_info&type=org',
@@ -15,10 +10,10 @@ export const Config = {
   cpsUnBindUrl: domain + '?m=admin&c=weixin&a=cps&type=unbind',
 
   indexUrl: domain + '?m=admin&c=weixin&a=index',
-  signatureUrl: domain + '?m=admin&c=weixin&a=signature',
-  uploadInitUrl: domain + '?m=admin&c=weixin&a=upload_init',
-  uploadSearchUrl: domain + '?m=admin&c=weixin&a=upload_search',
-  uploadSaveUrl: domain + '?m=admin&c=weixin&a=upload_save',
-  uploadDeleteUrl: domain + '?m=admin&c=weixin&a=upload_delete',
+
+  uploadInitUrl: domain + '?m=admin&c=weixin&a=upload&type=init',
+  uploadSearchUrl: domain + '?m=admin&c=weixin&a=upload&type=search',
+  uploadSaveUrl: domain + '?m=admin&c=weixin&a=upload&type=save',
+  uploadDeleteUrl: domain + '?m=admin&c=weixin&a=upload&type=delete',
 
 };
